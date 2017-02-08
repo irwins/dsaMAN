412ff15ee239aceff986bbf1b26996b79a6bf0ac
Irwin Strachan <irwins@users.noreply.github.com>
2017-02-08 14:13:12 +0100

    Add files via upload

---
+++ Public/New-ADMUser.ps1
@@ -0,0 +1,25 @@
+﻿Function New-ADMUser{
+   [CmdletBinding()]
+   param(
+      [String]$Identity,
+
+      [PSCustomObject]$Properties
+   )
+
+   Process{
+      if($PSBoundParameters.ContainsKey('Identity')){
+         if($PSBoundParameters.ContainsKey('Properties')){
+            Write-Verbose "PSBoundParameter Identity & Properties"
+            return [ADMUser]::new($Identity,$Properties) 
+         }
+         else{
+            Write-Verbose "PSBoundParameter Identity only"
+            return [ADMUser]::new($Identity)
+         }
+      }
+      else{
+         Write-Verbose 'No PSBoundParameter'
+         return [ADMUser]::new()
+      }
+   }
+}
